09be02037ec3e39bfa889dc023e9111ef0b707c0
alex_shatailov <alexandshatailov@gmail.com>
2021-10-26 22:24:13 +0300

    full refactoring

--- main.js
+++ main.js
@@ -1,35 +1,85 @@
-const flexboxModal = () => {
-    if(window.innerWidth < 600) return;
+const modalLinks = document.querySelectorAll('.testmodal__link')
+const body = document.querySelector('body')
+const lockPadding = document.querySelectorAll('.lock')
 
-    const overlay = document.getElementById('modalOverlay'),
-          modal = document.getElementById('modalWrapper'),
-          closeButton = document.getElementById('modalClose'),
-          showButton = document.getElementById('showModal'),
-          form = document.getElementById('form')
+let unlock = true
+const timeout = 800
 
-    const showModal = () => {
-        overlay.removeAttribute('style')
-        overlay.classList.remove('modal__hidden')
-        
-        modal.classList.remove('modal__hidden')
+if (modalLinks.length > 0) {
+    for (let modal of modalLinks) {
+        const modalLink = modal
+        console.log(modal)
+        modalLink.addEventListener('click', (e) => {
+            const modalName = modal.getAttribute('href').replace('#', '')
+            const curentModal = document.getElementById(modalName)
+            modalOpen(curentModal)
+            e.preventDefault()
+        })
     }
+}
 
-    const closeModal = () => {
-        modal.classList.add('modal__hidden')
-
-        overlay.removeAttribute('style')
-        overlay.classList.add('modal__hidden')
+const modalCloseIcon = document.querySelectorAll('.close__modal')
+if (modalCloseIcon.length > 0) {
+    for (let close of modalCloseIcon) {
+        const elem = close
+        elem.addEventListener('click', (e) => {
+            modalClose(elem.closest('.testmodal'))
+            e.preventDefault()
+        })
     }
+}
 
-    closeButton.addEventListener('click', () => closeModal())
+const modalOpen = (curentModal) => {
+    if (curentModal && unlock) {
+        const modalActive = document.querySelector('.testmodal')
+        console.log(modalActive)
+        if (curentModal) {
+            modalClose(modalActive, false)
+        } else {
+            bodyLock()
+        }
+        curentModal.classList.add('open')
+        curentModal.addEventListener('click', (e) => {
+            if (!e.target.closest('.modal__content')) {
+                modalClose(e.target.closest('modal'))
+            }
+        })
+    }
+}
 
-    overlay.addEventListener('click', () => closeModal())
+const modalClose = (modalActive, doUnlock = false) => {
+    if (unlock) {
+        modalActive.classList.remove('open')
+        if (doUnlock) {
+            bodyLock()
+        }
+    }
+}
 
-    form.addEventListener('submit', () => closeModal())
+const bodyLock = () => {
+    const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px'
+    if (lockPaddingValue > 0) {
+        for (let padLock of lockPadding) {
+            const elem = padLock
+            elem.getElementsByClassName.paddingRight = lockPaddingValue
+        }
+    }
+    body.style.paddingRight = lockPaddingValue
+    body.classList.add('lock')
 
-    showButton.addEventListener('click', () => showModal())
-};
-  
-document.addEventListener('DOMContentLoaded', () => {
-    flexboxModal();
-});
+    unlock = false
+    setTimeout(() => {
+        unlock = true
+    }, timeout)
+}
+
+const bodyUnLock = () => {
+    setTimeout(() => {
+        for (let padLock of lockPadding) {
+            const elem = padLock
+            elem.style.paddingRight = '0px'
+        }
+        body.style.paddingRight = '0px'
+        body.classList.remove('lock')
+    }, timeout)
+}
